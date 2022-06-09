d67cc03c191a311eb3573a237bb44d99dfb462e9
dannyyyspam <danielaliabdulkarim@gmail.com>
2022-06-08 20:10:04 -0500

    Call the API Function When Buttons Are Clicked

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,4 +1,5 @@
 var userFormEl = document.querySelector("#user-form");
+var languageButtonsEl = document.querySelector("#language-buttons");
 var nameInputEl = document.querySelector("#username");
 var repoContainerEl = document.querySelector("#repos-container");
 var repoSearchTerm = document.querySelector("#repo-search-term");
@@ -22,6 +23,18 @@ var formSubmitHandler = function(event) {
     console.log(event);
 };
 
+var buttonClickHandler = function(event) {
+    // get the language attribute from the clicked element
+    var language = event.target.getAttribute("data-language");
+  
+    if (language) {
+        getFeaturedRepos(language);
+        
+        // clear old content
+        repoContainerEl.textContent = "";
+    }
+};
+
 var getUserRepos = function(user) {
     // format the github api url
     var apiUrl = "https://api.github.com/users/" + user + "/repos";
@@ -43,6 +56,23 @@ var getUserRepos = function(user) {
     .catch(function(error) {
         // Notice this `.catch()` getting chained onto the end of the `.then()` method
         alert("Unable to connect to GitHub");
+    });
+};
+
+var getFeaturedRepos = function(language) {
+    // format the github api url
+    var apiUrl = "https://api.github.com/search/repositories?q=" + language + "+is:featured&sort=help-wanted-issues";
+  
+    // make a get request to url
+    fetch(apiUrl).then(function(response) {
+        // request was successful
+        if (response.ok) {
+            response.json().then(function(data) {
+                displayRepos(data.items, language);
+            });
+        } else {
+            alert("Error: " + response.statusText);
+        }
     });
 };
 
@@ -93,9 +123,8 @@ var displayRepos = function(repos, searchTerm) {
         // append container to the dom
         repoContainerEl.appendChild(repoEl);
     }
-    console.log(repos);
-    console.log(searchTerm);
 };
-
+  
 // add event listeners to forms
 userFormEl.addEventListener("submit", formSubmitHandler);  
+languageButtonsEl.addEventListener("click", buttonClickHandler);
